073f76995fbbbe01076c0d771ba67d79c23d27b3
Gordon Johnston <gordonj@foldedplane.com>
2021-01-27 21:09:33 +0000

    Add repro

---
+++ app/controllers/index.js
@@ -0,0 +1,14 @@
+import Controller from '@ember/controller';
+
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class IndexController extends Controller {
+
+    @service router;
+
+    @action
+    submit() {
+        this.router.transitionTo('home');
+    }
+}
